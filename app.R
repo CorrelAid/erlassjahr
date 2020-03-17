57eb3c330b6a6167307eea9859c3b9f8e593f4cd
Antonia Scherz <scherz.antonia@gmail.com>
2020-03-17 14:48:54 +0100

    Adding Reactive Data selection according to income level, Adding region and trend option (still need to be made reactive in the map).

--- app.R
+++ app.R
@@ -35,23 +35,46 @@ write.csv(sr19_erlassjahr, "./sr19_erlassjahr.csv")
 data <- read.csv("./sr19_erlassjahr.csv")
 data <- plyr::rename(data, c("country" = "NAME_DE"))
 
+# Combine data:
+datamap <- sp::merge(map, data, by.x="NAME_DE", duplicateGeoms=TRUE)
+
 
 ##--------------------------------------------------##
 ## User Interface Shiny                             ##
 ##--------------------------------------------------##
 
 ui <- fluidPage(
-  titlePanel(p("Erlassjahr app", style = "color:#3474A7")),
-  sidebarLayout(
+  titlePanel(title = h1("Erlassjahr app", style = "color:#3474A7", align = "center")),
+  #layout with main area and side bar
+   sidebarLayout( position = "left",
     sidebarPanel(
-      p("Made with Shiny"),
+      p("Auswahl Variablen"),
+  
+      # dropdown input selection Debt Indicator
       selectInput(
-        inputId = "variableselected",
+        inputId = "var_debtindikator",
         label = "Schuldenindikator Wählen",
-        choices = c("public_debt_bip", "public_debt_state_rev", "foreign_debt_exp", 
-                    "risk_excessive_debt", "external_debt_service_exp")
-      )
-    ),
+        choices = c("public_debt_bip2", "public_debt_state_rev2", "foreign_debt_exp2", 
+                    "risk_excessive_debt2", "external_debt_service_exp2")
+      ), # ADD AGGREGATED OPTION AS COLUM IN DATAFRAME
+      
+      # drop down input selection income category
+      selectInput(
+        inputId = "var_income",
+        label = "Einkommenskategorie wählen",
+        choices = c("L", "LM", "UM", "NA")
+      ),
+        # drop down input selection Region
+        selectInput("var_region", 
+                    "Region wählen", 
+                    choices = c("Afrika", "Europa", "Amerika", "Australien", "Asien")
+    ), # ADD REGION VARIABLE IN DATAFRAME
+    
+        # drop down input selection Trend anzeigen
+        selectInput("var_trend", 
+                    "Trend anzeigen", 
+                    choices = c("Ja", "Nein")
+      )),
     mainPanel(
       
       leafletOutput(outputId = "map") #,
@@ -64,23 +87,29 @@ ui <- fluidPage(
 # server()
 server <- function(input, output) {
   #output$table <- renderDT(data)
+  
+  filteredData <- reactive({
+    subset(data, income == input$var_income)
+    # add regional input when constructed columns with region input
+  })
+  
   output$map <- renderLeaflet({
     
     # Add data to map
-    datafiltered <- data
+    datafiltered <- filteredData()
     ordercounties <- match(map@data$NAME_DE, datafiltered$NAME_DE)
     map@data <- datafiltered[ordercounties, ]
-  
+    
     map$variableplot <- as.numeric(
-      map@data[, input$variableselected])
+      map@data[, input$var_debtindikator])
     
     # Create leaflet
-    pal <- colorBin("YlOrRd", domain = map$variableplot, bins = 4)
+    pal <- colorFactor("YlOrRd", domain = map$variableplot)
     
     labels <- sprintf("%s: %g", map$NAME_DE, map$variableplot) %>%
       lapply(htmltools::HTML)
     
-    l <- leaflet(map) %>%
+    l <- leaflet(map) %>% 
       addTiles() %>%
       addPolygons(
         fillColor = ~ pal(variableplot),
@@ -93,13 +122,20 @@ server <- function(input, output) {
         label = labels
       ) %>%
       setView( 0, 0, 2 ) %>%
-      leaflet::addLegend(
+      leaflet::addLegend(position = "bottomright",
         pal = pal, values = ~variableplot,
         opacity = 0.7, title = NULL
       )
   })
+  
+
 }
 
 # shinyApp()
 shinyApp(ui = ui, server = server)
 runApp('./app.R')
+
+
+################# USeful Codewaste:
+# c("Land mit geringem Einkommen", "Land mit mittlerem Einkommen im unteren Bereich", "Land mit mittlerem Einkommen im oberen Bereich", 
+#   "Land mit hohem Einkommen oder ohne Angaben"),
