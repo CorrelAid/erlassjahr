8bc730fe8b590c879040bfc872fb5085ecbafee1
Dennis Hammerschmidt <dennis_hammerschmidt@yahoo.com>
2020-04-14 15:34:26 +0200

    add risk overview data

--- data_cleaning.R
+++ data_cleaning.R
@@ -15,23 +15,32 @@ library(countrycode)
 library(tidyverse)
 library(magrittr)
 library(zoo)
+library(english)
 
-# Load data
+##-------------##
+##  Load Data  ##
+##-------------##
 
-# IMPORTANT: Trend variable will be read as POSCIX variable due to its pre-defined format in the orginial data source
-#            I manually changed this in the excel file to work with the data in R.
-
+##---- Main Data ----##
 sr20_erlassjahr <- read_xlsx("SR20 Überblickstabelle-191107.xlsx")
-# add Grundgesamtheit
-country_all <- read_xlsx("SR20 Überblickstabelle-191107.xlsx", sheet =  7)
 sr20_erlassjahr <- sr20_erlassjahr[1:129,1:22] # keep only relevant variables and rows that contain data points
+
+##---- Total Countries ----##
+country_all <- read_xlsx("SR20 Überblickstabelle-191107.xlsx", sheet =  7)
+country_all <- country_all[1:194,1]
+
+##---- Risk Overview ----##
+risks_overview <- read_xlsx("Risiken-Überblick.xlsx")
+risks_overview <- risks_overview[1:129,1:5]
 
 ##----------------------##
 ##  Basic Data Cleaning ##
 ##----------------------##
-# Rename variables 
+
+##---- Main Data ----##
+
+# Rename variables (not all variables could be renamed using dplyr::rename())
 names(sr20_erlassjahr)[11] <- "foreign_debt_exp"
-sr20_erlassjahr$foreign_debt_exp <- as.numeric(sr20_erlassjahr$foreign_debt_exp)
 names(sr20_erlassjahr)[14] <- "external_debt_service_exp"
 
 sr20_erlassjahr %<>% 
@@ -58,37 +67,69 @@ sr20_erlassjahr %<>%
          trend = `Trend`)
 
 # Turn character into numeric
+sr20_erlassjahr$foreign_debt_exp <- as.numeric(sr20_erlassjahr$foreign_debt_exp)
 sr20_erlassjahr$debt_situation <- as.numeric(sr20_erlassjahr$debt_situation)
 sr20_erlassjahr$trend <- as.numeric(sr20_erlassjahr$trend)
 
+# Clean country names
 sr20_erlassjahr$country <- gsub("\\*", "", sr20_erlassjahr$country)
-# Convert German country names to ISO3c codes
+
+# Convert country names (from German to ISO3)
 custom_match <- c("Moldawien" = "MDA")
-sr20_erlassjahr$country_A3 <- countrycode::countrycode(sr20_erlassjahr$country, 
+sr20_erlassjahr$ISO3 <- countrycode::countrycode(sr20_erlassjahr$country, 
                                                  "country.name.de", "iso3c", custom_match = custom_match)
 
+# Add regions as defined by erlassjahr.de
 sr20_erlassjahr$region <- NA
-sr20_erlassjahr$region <- ifelse(is.na(sr20_erlassjahr$country_A3), sr20_erlassjahr$country, NA)
+sr20_erlassjahr$region <- ifelse(is.na(sr20_erlassjahr$ISO3), sr20_erlassjahr$country, NA)
 sr20_erlassjahr$region <- zoo::na.locf(sr20_erlassjahr$region)
 
 # remove ambiguous states
-sr20_erlassjahr <- sr20_erlassjahr[!is.na(sr20_erlassjahr$country_A3), ]
+sr20_erlassjahr <- sr20_erlassjahr[!is.na(sr20_erlassjahr$ISO3), ]
 
-# add all country names
-country_all <- country_all[1:194,1]
-names(country_all)[1] <- "country_A3"
+##---- Total Countries ----##
+
+# Rename country name variable & add missing countries
+names(country_all)[1] <- "ISO3"
 country_all <- rbind(country_all, "Nordkorea")
 country_all <- rbind(country_all, "Kuba")
 
+# Convert country names (from English to ISO3)
 custom_match2 <- c("Eswatini" = "SWZ", "Kosovo" = "RKS", "Micronesia" = "FSM", "S†o Tom_ and PrÍncipe" = "STP", "Nordkorea" = "PRK", "Kuba" = "CUB")
-country_all$country_A3 <- countrycode::countrycode(country_all$country_A3, 
+country_all$ISO3 <- countrycode::countrycode(country_all$ISO3, 
                                                        "country.name", "iso3c", custom_match = custom_match2)
 
-sr20_erlassjahr <- merge(sr20_erlassjahr, country_all, by = "country_A3", all.y = TRUE)
-# create a variable that is 1 if the country is part of the sample and 0 otherwise
+# Merge total countries with main data
+sr20_erlassjahr <- merge(sr20_erlassjahr, country_all, by = "ISO3", all.y = TRUE)
 
+# Create a sample variable that is 1 if the country is part of the sample and 0 otherwise
 sr20_erlassjahr$sample <- ifelse(is.na(sr20_erlassjahr$country), 0, 1)
-sr20_erlassjahr$sample <- ifelse(sr20_erlassjahr$country_A3 == "CUB" | sr20_erlassjahr$country_A3 == "PRK", 1, sr20_erlassjahr$sample)
+# the two countries that were additionally added need to be coded manually to become part of the sample
+sr20_erlassjahr$sample <- ifelse(sr20_erlassjahr$ISO3 == "CUB" | sr20_erlassjahr$ISO3 == "PRK", 1, sr20_erlassjahr$sample)
+
+##---- Risk Overview ----##
+
+# Rename variables
+risks_overview %<>%
+  rename(ISO3 = `Land`,
+         extractivism = `Extraktivismus`,
+         fragility = `Fragiität`,
+         debt_prob = `Problematische Schuldenstruktur`,
+         vulnerability = `Vulnerabilität gegenüber Naturkatstrophen`)
+
+# Clean country names
+risks_overview$ISO3 <- gsub("\\*", "", risks_overview$ISO3)
+
+# Convert country names (from German to ISO3) -- we can re-use the custom_match from above as the list of countries
+# remains the same
+risks_overview$ISO3 <- countrycode::countrycode(risks_overview$ISO3, 
+                                             "country.name.de", "iso3c", custom_match = custom_match)
+
+# remove ambiguous states
+risks_overview <- risks_overview[!is.na(risks_overview$ISO3), ]
+
+# Merge total countries with main data
+sr20_erlassjahr <- merge(sr20_erlassjahr, risks_overview, by = "ISO3", all.x = TRUE)
 
 ##--------------------##
 ##  Rescale Variables ##
@@ -159,9 +200,6 @@ sr20_erlassjahr$debt_sit_cat <-
     )
   )
 
-# sr20_erlassjahr$debt_sit_cat <- factor(sr20_erlassjahr$debt_sit_cat)
-# levels(sr20_erlassjahr$debt_sit_cat) <- c("nicht kritisch", "leicht kritisch", "kritisch", "sehr kritisch")
-
 #######################
 ## Payment Situation ##
 #######################
@@ -172,69 +210,30 @@ payment_low <- c("IRQ", "KHM", "UKR")
 
 sr20_erlassjahr$payment_stop <-
   ifelse(
-    sr20_erlassjahr$country_A3 %in% payment_high,
+    sr20_erlassjahr$ISO3 %in% payment_high,
     "feuer_rot",
     ifelse(
-       sr20_erlassjahr$country_A3 %in% payment_med,
+       sr20_erlassjahr$ISO3 %in% payment_med,
       "feuer_orange",
-      ifelse(sr20_erlassjahr$country_A3 %in% payment_low, "feuer_grau", NA)
+      ifelse(sr20_erlassjahr$ISO3 %in% payment_low, "feuer_grau", NA)
     )
   )
 
-# rename country_A3 variable
-names(sr20_erlassjahr)[1] <- "ISO3"
+#####################
+## Risk Situations ##
+#####################
+
+risk_new <- function(var){
+  var <- ifelse(is.na(var), 0, 1)
+}
+
+sr20_erlassjahr$extractivism <- risk_new(sr20_erlassjahr$extractivism)
+sr20_erlassjahr$fragility <- risk_new(sr20_erlassjahr$fragility)
+sr20_erlassjahr$debt_prob <- risk_new(sr20_erlassjahr$debt_prob)
+sr20_erlassjahr$vulnerability <- risk_new(sr20_erlassjahr$vulnerability)
 
 ##------------------##
 ## Save the Dataset ##
 ##------------------##
 
 save(sr20_erlassjahr, file = "sr20_erlassjahr.RData")
-
-
-
-
-
-
-##---------------------------------##
-## Needed only for sr19_erlassjahr ##
-##---------------------------------##
-
-
-
-####################
-## Debt Situation ##
-####################
-
-## Create five categories for debt situation
-# 
-# # Function to recode the five indicators according to the coding scheme in Schuldenreport 2020 (page 17)
-# filter_recode <- function(var, filter) {
-#   var <- ifelse(var < filter[1],
-#                 0,
-#                 ifelse(
-#                   var >= filter[1] &
-#                     var <= filter[2],
-#                   1,
-#                   ifelse(var > filter[2] &
-#                            var <= filter[3],
-#                          2,
-#                          ifelse(var > filter[3],
-#                                 3,
-#                                 NA))
-#                 ))
-#   return(var)
-# }
-# 
-# # Define thresholds for each filter category
-# public_debt_bip_filter <- c(50, 75, 100)
-# public_debt_state_rev_filter <- c(200, 300, 400)
-# foreign_debt_bip_filter <- c(40, 60, 80)
-# foreign_debt_exp_filter <- c(150, 225, 300)
-# external_debt_service_exp_filter <- c(15, 22.5, 30)
-# 
-# # Apply filter categories and recode function to each of the five category variables
-# sr20_erlassjahr$public_debt_bip2 <- filter_recode(sr20_erlassjahr$public_debt_bip, public_debt_bip_filter)
-# sr20_erlassjahr$public_debt_state_rev2 <- filter_recode(sr20_erlassjahr$public_debt_state_rev, public_debt_state_rev_filter)
-# sr20_erlassjahr$foreign_debt_bip2 <- filter_recode(sr20_erlassjahr$foreign_debt_bip, foreign_debt_bip_filter)
-# sr20_erlassjahr$foreign_debt_exp2 <- filter_recode(sr20_erlassjahr$foreign_debt_exp, foreign_debt_exp_filter)
-# sr20_erlassjahr$external_debt_service_exp2 <- filter_recode(sr20_erlassjahr$external_debt_service_exp, external_debt_service_exp_filter)
